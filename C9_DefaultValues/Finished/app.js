6eb50a20b1d2cf48ebaee1d8672644be9a6565a7
Andrew <andrew.j.heinke@gmail.com>
2016-12-08 22:12:35 -0600

    object literals lesson

--- C9_DefaultValues/Finished/app.js
+++ C9_DefaultValues/Finished/app.js
@@ -1,6 +1,6 @@
 function greet(name) {
-    name = name || '<Your name here>';
-    console.log('Hello ' + name);    
+    name = name || 'Andrew';
+    console.log('Hello ' + name);
 }
 
 greet('Tony');
